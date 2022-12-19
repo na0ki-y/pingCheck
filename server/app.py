cd8f9b59c7031bf7d55376e0c1e4dcec1c9d58ed
na0ki_mac <wehallo53.en@gmail.com>
2022-12-19 15:40:57 +0900

    secrets

--- server/app.py
+++ server/app.py
@@ -4,6 +4,8 @@ def main():
 
     st.write("secrets_sample-a:", st.secrets["secrets_sample"]["a"])
     st.write("secrets_sample-b:", st.secrets["secrets_sample"]["b"])
+    st.write("secrets_sample-b:", st.secrets["secrets_sample"]["c"])
+
 
     st.markdown("firebaseの秘密鍵はJSONファイルなのでTOMLに変換して同様にlocal/deployにペーストすれば良い[Streamlit ❤️ Firestore (continued)](https://blog.streamlit.io/streamlit-firestore-continued/)")
     st.markdown("# sesstion state counter")
