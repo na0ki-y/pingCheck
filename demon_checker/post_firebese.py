df45a23e6fe8a3fe685e3caf7c44daf4b9ffaba1
na0ki_mac <wehallo53.en@gmail.com>
2022-12-20 09:51:01 +0900

    firestore read and add OK

--- demon_checker/post_firebese.py
+++ demon_checker/post_firebese.py
@@ -1,15 +1,34 @@
 import toml
 import json
-from google.oauth2 import service_account
-from google.cloud import firestore
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 def main():
     with open("../server/.streamlit/secrets.toml") as toml_file:
         toml_text = toml_file.read()
         key_dict = json.loads(toml.loads(toml_text)["firebase_key"])
-        creds = service_account.Credentials.from_service_account_info(key_dict)
-        db = firestore.Client(credentials=creds, project="pingCheckDB")
+        creds = credentials.Certificate(key_dict)
+        firebase_admin.initialize_app(creds)
+        db = firestore.client()        
+        ##############################post
+        try:
+            # Firestoreのコレクションにアクセス
+            doc_ref = db.collection('ip_mac')
+            # Firestoreにドキュメントidを指定しないで１つづつニュースを保存
+            doc_ref.add({
+                'ip': "192.449.596",
+                'mac': "aa.aa.aa.rr",
+                'user': "user5",
+            })
+        except:
+            print('error')
+        ##############################read
         print(db.collection('ip_mac').document("2").path)
 
+        docs = db.collection(u'ip_mac').stream()
+
+        for doc in docs:
+            print(f'{doc.id} => {doc.to_dict()}')
         #for doc in db.collection('ip_mac').stream():
         #        print('{} => {}'.format(doc.id, doc.to_dict()))
 
--- server/app.py
+++ server/app.py
@@ -1,20 +1,34 @@
 import streamlit as st
 import json
-from google.oauth2 import service_account
-from google.cloud import firestore
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 def main():
     st.title("pingChecker")
 
     st.write("secrets_sample-a:", st.secrets["secrets_sample"]["a"])
     st.write("secrets_sample-b:", st.secrets["secrets_sample"]["b"])
+    if "docs" not in st.session_state:
+        key_dict = json.loads(st.secrets["firebase_key"]) 
+        #creds = service_account.Credentials.from_service_account_info(key_dict)
+        creds = credentials.Certificate(key_dict)
+        firebase_admin.initialize_app(creds)    
+        db = firestore.client()        
+        st.session_state.docs = db.collection(u'ip_mac').stream()
+    ##############################post
+    try:
+        pass
+        #doc_ref = db.collection('ip_mac')
+        #doc_ref.add({'ip': "192.449.596",'mac': "aa.aa.aa.rr",'user': "user3",})
+    except:
+        print('error')
+    ##############################read
 
-    key_dict = json.loads(st.secrets["firebase_key"]) 
-    creds = service_account.Credentials.from_service_account_info(key_dict)
-    db = firestore.Client(credentials=creds, project="pingCheckDB")
-    st.write(db.collection('ip_mac').document("2").path)
 
-    #for doc in db.collection('ip_mac').stream():
-        #        print('{} => {}'.format(doc.id, doc.to_dict()))
+
+        
+    for doc in st.session_state.docs:
+        st.markdown(f'{doc.id} => {doc.to_dict()}')
 
     st.markdown("firebaseの秘密鍵はJSONファイルなのでTOMLに変換して同様にlocal/deployにペーストすれば良い[Streamlit ❤️ Firestore (continued)](https://blog.streamlit.io/streamlit-firestore-continued/)")
     st.markdown("# sesstion state counter")
