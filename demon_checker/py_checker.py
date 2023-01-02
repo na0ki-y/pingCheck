a1b5bbbe9b6cae51b858a62cbd44e7b492c97f11
na0ki_mac <wehallo53.en@gmail.com>
2023-01-02 12:16:08 +0900

    demon loop

--- demon_checker/py_checker.py
+++ demon_checker/py_checker.py
@@ -5,6 +5,15 @@ from utiles_firebese import firebase_read
 from utiles_firebese import firebase_post
 private_addr_list=["192.168.11","192.168.1"]
 def pingGetmacAll(flag_print=True):
+    """ 
+    ローカルネット内にいるすべてのデバイスのMAC Addressを返す
+    --
+    input:flag_print=True
+    output:mac_list
+    --
+    ローカルネットのすべてのipアドレスが存在するかpingして、arpでMAC Addressの取得をする
+
+    """
     st=os.popen("ipconfig getifaddr en0")
     addr=st.read()
     private_addr=".".join(addr.split(".")[:3])#"192.168.11."
@@ -20,6 +29,15 @@ def pingGetmacAll(flag_print=True):
 
     return mac_list
 def check_user(user_list={"user":["Allice","Bob","Char"],"mac":['3X:XX:XX:XX:XX:XX','23:XX:XX:XX:XX:XX','35:XX:XX:XX:XX:XX']},exit_mac_list=['51:XX:XX:XX:XX:XX','11:XX:XX:XX:XX:XX','12:XX:XX:XX:XX:XX','23:XX:XX:XX:XX:XX']):
+    """ 
+    DBに登録されたユーザ(mac addr)が取得済みローカル内デバイスとマッチするかチェックする
+    --
+    input:user_list(登録されたユーザ),exit_max_list(ローカル内デバイス)
+    output:onoff_result
+    --
+    総当りで２重ループで確認
+
+    """
     onoff_result=[]
     for i,u_m in enumerate(user_list["mac"]):
         flag_onoff=False
@@ -32,15 +50,29 @@ def check_user(user_list={"user":["Allice","Bob","Char"],"mac":['3X:XX:XX:XX:XX:
     return onoff_result
 
 def one_check_and_post(db):
-    user_list=firebase_read(db)
-    exit_mac_list=pingGetmacAll()
-    onoff_result=check_user(user_list,exit_mac_list)
-    firebase_post(db,collection_name="log_db",post_field={"time":time.time(),'user_list':user_list["user"],'result': onoff_result,})
+    """ 
+    １回のチェックの一連の動作
+    --
+    input:db
+    output:なし(firebase_postでpost)
+    --
+    firebase_read()でDBのユーザを取得し、exit_mac_list()でローカル内デバイスを取得
+    check_user()で確認そしてfirebase_post()でpost
+    """
+    user_list=firebase_read(db)#DBのユーザを取得
+    exit_mac_list=pingGetmacAll()#ローカル内のデバイスのmacを取得
+    onoff_result=check_user(user_list,exit_mac_list)#DBに登録されたユーザ(mac addr)が取得済みローカル内デバイスとマッチするかチェックする
+    firebase_post(db,collection_name="log_db",post_field={"time":time.time(),'user_list':user_list["user"],'result': onoff_result,})#結果のpost
     print(time.time(),user_list["user"],onoff_result)
 
 def main():
     db=firebase_conect()
-    one_check_and_post(db)
+    i=0
+    while True:
+        print(i)
+        one_check_and_post(db)
+        time.sleep(600)#n秒ごとに実行
+        i+=1
     
 if __name__ == '__main__':
     main()
--- demon_checker/utiles_firebese.py
+++ demon_checker/utiles_firebese.py
@@ -6,6 +6,12 @@ def main():
     pass
 
 def firebase_conect():
+    """ 
+    DB(firebase)と接続
+    --
+    input:なし
+    output:db
+    """
     toml_file = open('../server/.streamlit/secrets.toml')
     toml_text = toml_file.read()
     key_dict = json.loads(toml.loads(toml_text)["firebase_key"])
@@ -14,8 +20,14 @@ def firebase_conect():
     toml_file.close()
     return db
         
-        
 def firebase_post(db,collection_name="user",post_field={'mac': "aa.aa.aa.rr",'name': "Kei",}):
+    """ 
+    DB(firebase)にpost
+    --
+    input:db,collection_name,post_field
+    output:なし(post)
+    collection_nameへpost_fieldをpost
+    """
     doc_ref = db.collection(collection_name)
     ##############################post
     try:
@@ -23,6 +35,13 @@ def firebase_post(db,collection_name="user",post_field={'mac': "aa.aa.aa.rr",'na
     except:
         print('error_post')
 def firebase_read(db,collection_name="user",flag_print=False):
+    """ 
+    DB(firebase)を読み込む
+    --
+    input:db,collection_name
+    output:read_result
+    collection_nameの内容をread_result
+    """
     posts_ref = db.collection(collection_name)
     read_result={"user":[],"mac":[]}
     for doc in posts_ref.stream():
