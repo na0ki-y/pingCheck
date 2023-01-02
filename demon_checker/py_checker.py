a59ea68ebaa02f42dc624cbea01b2b4776bd3ae2
na0ki_mac <wehallo53.en@gmail.com>
2023-01-02 11:59:26 +0900

    プライベートアドレス空間の変更対応

--- demon_checker/py_checker.py
+++ demon_checker/py_checker.py
@@ -3,12 +3,15 @@ import time
 from utiles_firebese import firebase_conect
 from utiles_firebese import firebase_read
 from utiles_firebese import firebase_post
-private_addr_list=["192.168.11.","192.168.1."]
-def pingGetmacAll(private_addr=private_addr_list[1],flag_print=True):
+private_addr_list=["192.168.11","192.168.1"]
+def pingGetmacAll(flag_print=True):
+    st=os.popen("ipconfig getifaddr en0")
+    addr=st.read()
+    private_addr=".".join(addr.split(".")[:3])#"192.168.11."
     mac_list=[]
     for i in range(0,255):
-        stream = os.popen('ping -c 1 -W 10 {}{}'.format(private_addr,i))
-        stream = os.popen('arp {}{}'.format(private_addr,i))
+        stream = os.popen('ping -c 1 -W 10 {}.{}'.format(private_addr,i))
+        stream = os.popen('arp {}.{}'.format(private_addr,i))
         output = stream.read()
         if "at" in output and not("(incomplete)" in output.split("at")[1]):#"Found"
             mac_list.append(output.split("at")[1].split(" ")[1])
@@ -30,7 +33,7 @@ def check_user(user_list={"user":["Allice","Bob","Char"],"mac":['3X:XX:XX:XX:XX:
 
 def one_check_and_post(db):
     user_list=firebase_read(db)
-    exit_mac_list=pingGetmacAll(private_addr=private_addr_list[1])
+    exit_mac_list=pingGetmacAll()
     onoff_result=check_user(user_list,exit_mac_list)
     firebase_post(db,collection_name="log_db",post_field={"time":time.time(),'user_list':user_list["user"],'result': onoff_result,})
     print(time.time(),user_list["user"],onoff_result)
